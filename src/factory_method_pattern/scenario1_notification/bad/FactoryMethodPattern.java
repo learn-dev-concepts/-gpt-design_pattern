9cc898bc82bd07822355966cb0f734808b8c6f13
devsince2021 <dev.ryu2021@gmail.com>
2024-01-21 01:09:50 +0900

    프로젝트 생성

---
+++ src/factory_method_pattern/scenario1_notification/bad/FactoryMethodPattern.java
@@ -0,0 +1,8 @@
+package factory_method_pattern.scenario1_notification.bad;
+
+public class FactoryMethodPattern {
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+  }
+}
